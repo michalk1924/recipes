7f31ce25134164dc4758e9db3457be593496a68b
Yael Koppel <yaelkoppel1@gmail.com>
2024-11-12 15:42:36 +0200

    commit

--- src/components/RecipeTag/RecipeTag.tsx
+++ src/components/RecipeTag/RecipeTag.tsx
@@ -12,13 +12,18 @@ interface RecipeTagProps {
 const RecipeTag: React.FC<RecipeTagProps> = ({ recipe, showRecipePopup }) => {
     return (
         <div className={styles.recipeTag}>
+
             <img src={recipe.image_url} alt={recipe.name} className={styles.recipeImage} />
-            <div className={styles.recipeDetails}>
+            <p onClick={() => showRecipePopup(recipe)} className={styles.recipeTitle}>{recipe.name}</p>
+
+
+           {/*  <div className={styles.recipeDetails}>
                 <h2 className={styles.recipeTitle}>{recipe.name}</h2>
                 <p className={styles.ingredients}><strong>Ingredients:</strong> {recipe.ingredients}</p>
-            </div>
-            <button onClick={() => showRecipePopup(recipe)}>View Recipe</button>
-        </div>
+            </div> */}
+{/*             <button onClick={() => showRecipePopup(recipe)}>View Recipe</button>
+ */}   
+      </div>
     );
 };
 
