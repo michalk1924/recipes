bc7c3f8768396637a35778252e2075872018b702
Yael Koppel <yaelkoppel1@gmail.com>
2024-11-12 10:59:19 +0200

    commit

--- src/app/recipeDetails/[id]/page.tsx
+++ src/app/recipeDetails/[id]/page.tsx
@@ -4,14 +4,18 @@ import { usePathname } from "next/navigation";
 import styles from "../recipeDetails.module.css";
 import recipesService from "@/services/recipes";
 import { Recipe } from "@/types";
+import useCategoryOptions from '@/categoriesZustand'
 
 const RecipeDetails: React.FC = () => {
     const pathname = usePathname();
     const id = pathname?.split("/").pop();
 
     const [recipe, setRecipe] = useState<Recipe | null>(null);
+    const [category, setCategory] = useState<string | undefined>(undefined);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const { categories, initializeCategories } = useCategoryOptions();
+
 
     useEffect(() => {
         if (!id) return;
@@ -31,41 +35,60 @@ const RecipeDetails: React.FC = () => {
         fetchRecipe();
     }, [id]);
 
+    useEffect(() => {
+        try{
+            initializeCategories();
+        }
+        catch(error){
+            console.error(error);
+        }
+
+    const recipeCategory = categories?.find(
+        (category) => category.category_id === recipe?.category
+    );
+    setCategory(recipeCategory?.category_name || '');
+
+    console.log(categories)
+},[id]);
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error}</p>;
     if (!recipe) return <p>No recipe found.</p>;
 
+    
+
     return (
-        <div className={styles.recipeDetails}>
-            <div className={styles.recipeProfile}>
-                <img
-                    src={recipe.image_url}
-                    alt={recipe.name}
-                    className={styles.recipeImage}
-                />
-                <div className={styles.recipeTitle}>
-                    <h1 className={styles.recipeName}>{recipe.name}</h1>
-
-                    <h2 className={styles.recipeCategory}>{recipe.category}</h2>
-
-                    <p className={styles.ingredients}>
-                        <strong>Ingredients:</strong>
-                        {recipe.ingredients.map((ingredient, index) => (
-                            <span key={index}>
-                                <br />
-                                {ingredient}
-                            </span>
-                        ))}
-                    </p>
+        <div className="centerContainer">
+            <div className={styles.recipeDetails}>
+                <div className={styles.recipeProfile}>
+                    <img
+                        src={recipe.image_url}
+                        alt={recipe.name}
+                        className={styles.recipeImage}
+                    />
+                    <div className={styles.recipeTitle}>
+                        <h1 className={styles.recipeName}>{recipe.name}</h1>
+                        <h2 className={styles.recipeCategory}>{category}</h2>
+                        <p className={styles.ingredients}>
+                            <strong>Ingredients:</strong>
+                            {recipe.ingredients.map((ingredient, index) => (
+                                <span key={index}>
+                                    <br />
+                                    {ingredient}
+                                </span>
+                            ))}
+                        </p>
+                    </div>
                 </div>
-
+                <p className={styles.instructions}>
+                    <strong>Instructions:</strong>
+                    <br /> {recipe.instructions}{" "}
+                </p>
             </div>
-            <p className={styles.instructions}>
-                <strong>Instructions:</strong>
-                <br /> {recipe.instructions}{" "}
-            </p>
         </div>
     );
+    
+    
 };
 
 export default RecipeDetails;
