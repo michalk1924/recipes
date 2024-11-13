bddb2535d4be893adac5982d3526f9ce90477ce7
michalk1924 <michalk1924@gmail.com>
2024-11-13 02:30:43 +0200

    style and paging

--- src/components/RecipePopup/RecipePopup.tsx
+++ src/components/RecipePopup/RecipePopup.tsx
@@ -7,29 +7,31 @@ import { useRouter } from 'next/navigation';
 import styles from './RecipePopup.module.css'
 
 interface RecipePopupProps {
-    recipe: Recipe;
-  }
-  
-  const RecipePopup: React.FC<RecipePopupProps> = ({ recipe }) => {
+  recipe: Recipe;
+}
 
-    const router = useRouter();
-    const { categories } = useCategoryOptions();
-    const categoryName = categories.find((category) => category.category_id === recipe.category)?.category_name || "Unknown Category";
-    const shortInstructions = recipe.instructions.substring(0, 100) + '...';
+const RecipePopup: React.FC<RecipePopupProps> = ({ recipe }) => {
 
-    const readMore = () => {
-        router.push(`/recipeDetails/${recipe._id}`);
-    };
+  const router = useRouter();
+  const { categories } = useCategoryOptions();
+  const categoryName = categories.find((category) => category.category_id === recipe.category)?.category_name || "Unknown Category";
+  const shortInstructions = recipe.instructions.substring(0, 100) + '...';
 
-    return (
-        <div className={styles.recipePopup}>
-        <img src={recipe.image_url} alt={recipe.name} />
-        <h1>{recipe.name}</h1>
-        <h2>{categoryName}</h2>
-        <p>{shortInstructions}</p>
-        <button onClick={readMore}>Read More</button>
+  const readMore = () => {
+    router.push(`/recipeDetails/${recipe._id}`);
+  };
+
+  return (
+    <div className={styles.recipePopup}>
+      <img src={recipe.image_url} alt={recipe.name} className={styles.recipeImage} />
+      <div className={styles.recipeContent}>
+        <h1 className={styles.recipeName}>{recipe.name}</h1>
+        <h2 className={styles.recipeCategory}>{categoryName}</h2>
+        <p className={styles.recipeDescription}>{shortInstructions}</p>
+        <button onClick={readMore} className={styles.readMoreButton}>Read more</button>
       </div>
-    )
+    </div>
+  )
 }
 
 export default RecipePopup
--- src/components/Recipes/Recipes.tsx
+++ src/components/Recipes/Recipes.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import recipesService from '@/services/recipes';
 import { Recipe } from "@/types";
 import RecipeTag from "@/components/RecipeTag/RecipeTag";
 import Categories from "@/components/Categories/Categories";
 import styles from './Recipes.module.css';
 import RecipePopup from '@/components/RecipePopup/RecipePopup';
-import { ONEMINUTE } from '@/app/lib/consts';
+import { ONEMINUTE, PAGESIZE } from '@/app/lib/consts';
 import { getFromStorage, saveToStorage } from '@/app/lib/storage';
 
 export default function Recipes() {
@@ -20,9 +20,10 @@ export default function Recipes() {
   const [showRecipePopup, setShowRecipePopup] = useState<boolean>(false);
   const [recipePopupDetails, setRecipePopupDetails] = useState<Recipe>();
   const [showFavoritesRecipes, setShowFavoritesRecipes] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
         setIsLoading(true);
@@ -31,126 +32,146 @@ export default function Recipes() {
           const storedTime = parseInt(savedTimefromStorage, 10);
           const currentTime = Date.now();
           if (currentTime - storedTime > ONEMINUTE) {
-            getRecipesFromServer();
+            await getRecipesFromServer();
           }
           else {
-            let data;
-            data = getFromStorage(`recipes`);
-            if (category) {
-              data = data.filter((r: Recipe) => r.category === category);
-            }
+            let data = getFromStorage(`recipes`);
             setRecipes(data);
           }
+        } else {
+          await getRecipesFromServer();
         }
-        else {
-          getRecipesFromServer();
-        }
+      } catch (err) {
+        console.error(err);
+        setRecipes([]);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getRecipesFromServer = async () => {
+    if (!hasMore) return;
+    const newRecipes = await recipesService.getAllRecipes(page);
+    if (newRecipes.length === 0) {
+      setHasMore(false);
+      return;
+    }
+    let data = [...recipes, ...newRecipes];
+    const isHasMore = newRecipes.length === PAGESIZE;
+    setRecipes(data);
+    setHasMore(isHasMore);
+    saveToStorage(`recipes`, data);
+    saveToStorage(`savedTime`, Date.now().toString());
+  };
+
+  useEffect(() => {
+    if (page > 1) {
+      try {
+        getRecipesFromServer();
       }
       catch (err) {
         console.error(err);
-        setRecipes([]);
-      }
-      finally {
-        setIsLoading(false);
       }
     }
+  }, [page]);
 
-    const getRecipesFromServer = async () => {
-      let data;
-      data = await recipesService.getAllRecipes();
-      if (category) {
-        data = data.filter(data.filter((r: Recipe) => r.category === category));
+  const handleScroll = useCallback(() => {
+    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 50 && hasMore && !isLoading) {
+      if (recipes.length % PAGESIZE === 0) {
+        setPage(prevPage => prevPage + 1);
       }
-      saveToStorage(`recipes`, data);
-      saveToStorage(`savedTime`, Date.now().toString());
-      setRecipes(data);
     }
-
-    fetchData();
-
-  }, []);
-
+  }, [isLoading, hasMore, recipes]);
 
   useEffect(() => {
-    let data: Recipe[];
-    data = showFavoritesRecipes ? recipes.filter((r: Recipe) => r?.is_favorite) : recipes;
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  useEffect(() => {
+    let data: Recipe[] = recipes;
+
+    if (showFavoritesRecipes) {
+      data = data.filter((r: Recipe) => r?.is_favorite);
+    }
     if (category) {
       data = data.filter((r: Recipe) => r.category === category);
     }
-    data = data.filter(r => r.name.toLocaleLowerCase()
-      .includes(filterInput.toLocaleLowerCase()));
+    data = data.filter(r => r.name.toLowerCase().includes(filterInput.toLowerCase()));
+
     setFilteredRecipes(data);
   }, [recipes, showFavoritesRecipes, filterInput, category]);
 
   const showRecipePopupF = (recipe: Recipe) => {
     setRecipePopupDetails(recipe);
     setShowRecipePopup(true);
-  }
+  };
 
   const updateFavorites = (recipe_id: string) => {
     try {
-      
       const recipe = recipes.find(r => r._id === recipe_id);
       if (recipe) {
         recipe.is_favorite = !recipe.is_favorite;
         setRecipes(recipes => recipes.map(r => r._id !== recipe_id ? r : recipe));
         saveToStorage(`recipes`, recipes);
         recipesService.updateRecipe(recipe_id, recipe);
-      }
-      else {
+      } else {
         console.error("Recipe not found in the list.");
       }
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
-      
-     <h1 className={styles.title}>Recipes</h1>
-
       <div className={styles.topSection}>
-
-      <Categories setCategory={setCategory} />
-
-      <input
-        type="text"
-        placeholder="Search..."
-        value={filterInput}
-        onChange={e => setFilterInput(e.target.value)}
-        className={styles.searchInput}/>
-        
+        <Categories setCategory={setCategory} />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterInput}
+          onChange={e => setFilterInput(e.target.value)}
+          className={styles.searchInput}
+        />
       </div>
 
-      <button onClick={() => { showFavoritesRecipes ? setShowFavoritesRecipes(false) : {} }}>
-        all
-      </button>
+      <div className={styles.buttons}>
+        <button
+          onClick={() => setShowFavoritesRecipes(!showFavoritesRecipes)}
+          className={`${styles.filterButton} ${!showFavoritesRecipes ? styles.active : ''}`}
+        >
+          All Recipes
+        </button>
+        <button
+          onClick={() => setShowFavoritesRecipes(!showFavoritesRecipes)}
+          className={`${styles.filterButton} ${showFavoritesRecipes ? styles.active : ''}`}
+        >
+          Favorites
+        </button>
+      </div>
 
-      <button onClick={() => { !showFavoritesRecipes ? setShowFavoritesRecipes(true) : {} }}>
-        favorites
-      </button>
-
-      {!isLoading && <div className={styles.recipesGrid} >
-        {filteredRecipes
-          .map(recipe => (
-            <RecipeTag key={recipe._id} showRecipePopup={showRecipePopupF} recipe={recipe}
-              updateFavorites={updateFavorites} />
+      {!isLoading && (
+        <div className={styles.recipesGrid}>
+          {filteredRecipes.map(recipe => (
+            <RecipeTag
+              key={recipe._id}
+              showRecipePopup={showRecipePopupF}
+              recipe={recipe}
+              updateFavorites={updateFavorites}
+            />
           ))}
-      </div>}
+        </div>
+      )}
 
-      {isLoading && <div>Loading...</div>}
-      {showRecipePopup && recipePopupDetails && <RecipePopup recipe={recipePopupDetails} />}
-      
+      {isLoading && <div className={styles.loading}>Loading...</div>}
+
+      {showRecipePopup && recipePopupDetails && (
+        <RecipePopup recipe={recipePopupDetails} />
+      )}
     </div>
   );
-
 }
-
-
-//paging
-//favories
-//gitgnore
-
-//r-add
