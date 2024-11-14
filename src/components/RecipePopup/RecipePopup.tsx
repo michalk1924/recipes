fbe8003aa3ef366e66bdc17f626a098027d7b137
michalk1924 <michalk1924@gmail.com>
2024-11-14 09:47:34 +0200

    add button to go home

--- src/components/RecipePopup/RecipePopup.tsx
+++ src/components/RecipePopup/RecipePopup.tsx
@@ -27,15 +27,22 @@ const RecipePopup: React.FC<RecipePopupProps> = ({ recipe, updateFavorites }) =>
   return (
     <div className={styles.recipePopup}>
       <img src={recipe.image_url} alt={recipe.name} className={styles.recipeImage} />
+      
       <div className={styles.recipeContent}>
         <h1 className={styles.recipeName}>{recipe.name}</h1>
         <h2 className={styles.recipeCategory}>{categoryName}</h2>
         <p className={styles.recipeDescription}>{shortInstructions}</p>
-        <button onClick={readMore} className={styles.readMoreButton}>Read more</button>
-        <button onClick={() => updateFavorites(recipe._id)}>
-          {recipe.is_favorite && <span><FaStar className={styles.yellowIcon} /></span>}
-          {!recipe.is_favorite && <span><FaRegStar className={styles.yellowIcon} /></span>}
-        </button>
+
+        <div className={styles.footerButtons}>
+
+          <button onClick={readMore} className={styles.readMoreButton}>Read more</button>
+          <button onClick={() => updateFavorites(recipe._id)}>
+            {recipe.is_favorite && <span><FaStar className={styles.yellowIcon} /></span>}
+            {!recipe.is_favorite && <span><FaRegStar className={styles.yellowIcon} /></span>}
+          </button>
+
+        </div>
+
       </div>
     </div>
   )
